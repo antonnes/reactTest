a6a5e70fae7bb3182203b704dd48e181c48e5dce
Anthony Nikolov <tonnes.nk@gmail.com>
2019-12-16 12:09:46 +0200

    fixed dates, improved filtering

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -2,78 +2,150 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './datagrid.css';
 import Data from '../../data/mockup.json';
-import Popover from '../popover/popover';
 
 // Plugins
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEllipsisV, faSort, faSortUp, faSortDown, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisV, faSort, faSortUp, faSortDown, faTrashAlt, faTimes, faSearch } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faEllipsisV, faSort, faSortUp, faSortDown, faTrashAlt);
+library.add(faEllipsisV, faSort, faSortUp, faSortDown, faTrashAlt, faTimes, faSearch);
 
 Modal.setAppElement('#root');
 
 class Datagrid extends React.Component {
 
-  constructor() {
-    super();
-   
-    this.state = {
-      showModal: false,
-      indexToDelete: null,
-      fields: {
-          Id: null,
-          title: null,
-          category: null,
-          quantity: null,
-          description: null,
-          createdDate: null
-      },
-      products: Data.map((product) => {
-          //product.createdDate = new Intl.DateTimeFormat('en-US').format(new Date(product.createdDate)).join(" - ");
-          return product;
-      })
-    };
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
-  }
-  componentDidMount () {
-    const products = this.props.products;
-    console.log(this.props);
-  }
-  handleOpenModal (index) {
-    this.setState({ showModal: true, indexToDelete: index });
-  }  
-  handleCloseModal () {
-    this.setState({ showModal: false, indexToDelete: null });
-  }
-  handleOutsideClick(e) {
-    // ignore clicks on the component itself
-    if (this.node.contains(e.target)) {
-      return;
+    constructor() {
+        super();
+        const products = Data.map((product) => {
+            product.creationDate = this.FormatDate(product.creationDate);
+            return product;
+        });
+        this.state = {
+            showModal: false,
+            indexToDelete: null,
+            searchString: '',
+            clearVisible: false,
+            searchInTitle: true,
+            searchInDescription: false,
+            searchInCategory: false,
+            products: products,
+            filteredProducts: products
+        };
+        this.OpenModal = this.OpenModal.bind(this);
+        this.CloseModal = this.CloseModal.bind(this);
+        this.ClearFilter = this.ClearFilter.bind(this);
+        this.CheckBoxChanged = this.CheckBoxChanged.bind(this);
     }
-    
-    this.handleClick();
-  }
-  deleteProduct() {
-      console.log(this.state.indexToDelete);
-      let products = [...this.state.products];
-      products.splice(this.state.indexToDelete,1);
-      this.setState({products: products});
-      this.handleCloseModal();
-  }
-  filter(string) {
-      let result = this.state.products.filter(product => product.title.includes('tempor'));
-      this.setState({
-        products: result
-    });
-  }
+    componentDidMount () {
+        const products = this.props.products;
+        console.log(this.props);
+    }
+    OpenModal (index) {
+        this.setState({ showModal: true, indexToDelete: index });
+    }  
+    CloseModal () {
+        this.setState({ showModal: false, indexToDelete: null });
+    }
+    DeleteProduct() {
+        let products = [...this.state.products];
+        let filteredProducts = [...this.state.filteredProducts];
+        products.splice(this.state.indexToDelete,1);
+        filteredProducts.splice(this.state.indexToDelete,1);
+        this.setState({products: products, filteredProducts: filteredProducts});
+        this.CloseModal();
+    }
+    Filter() {
+        let searchString = this.state.searchString;
+        if(searchString != null) {
+            let result = this.state.products.filter(product => {
+                let searchOptions = {title: this.state.searchInTitle, description: this.state.searchInDescription, category: this.state.searchInCategory};
+                for (let index in searchOptions) {
+                    if(product[index].includes(searchString)) {
+                        return product;
+                    }                    
+                }
+            });
+            this.setState({
+                filteredProducts: result
+            });
+        } else {
+            this.setState({
+                filteredProducts: this.state.products
+            });
+        }
+
+    }
+    ClearFilter() {
+        this.setState({
+            filteredProducts: this.state.products,
+            searchString: ''
+        });
+    }
+    changeHandler(key) {
+                
+        return function (e) {
+            var state = {};
+            state[key] = e.target.value;
+            this.setState(state);
+        }.bind(this);
+
+    }
+    CheckBoxChanged(key) {
+         return function (e) {
+            var state = {};
+            state[key] = !this.state[key];
+            this.setState(state);
+        }.bind(this);
+        
+    }
+    FormatDate(date) {
+        let year = new Date(date).getFullYear();
+        let month =  new Date(date).getMonth();
+        let day =  new Date(date).getDate();
+        return `${day}.${month}.${year}`;
+    }
   render() {
     return <React.Fragment>
     <div className="control-panel">        
-        <input type="text"/>
-        <button className="btn-submit" onClick={() => {this.filter()}}>Search</button>        
+        <div className="clearfix">
+            <div className="search-box">
+                <span className="icon search">
+                    <FontAwesomeIcon icon="search"/>
+                </span>
+                <input type="text" value={this.state.searchString} onChange={this.changeHandler('searchString')}/>
+                {this.state.searchString && <span className="icon clear" onClick={this.ClearFilter}>
+                    <FontAwesomeIcon icon="times"/>
+                </span> }
+            </div>
+            <button className="btn-submit" onClick={() => {this.Filter()}}>Search</button>
+        </div>
+        <div className="search-options">
+            <label>
+                <input
+                    type="checkbox"
+                    checked={this.state.searchInTitle}
+                    onChange={this.CheckBoxChanged('searchInTitle')}
+                />
+                <span>Search in title</span>
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={this.state.searchInDescription}
+                    onChange={this.CheckBoxChanged('searchInDescription')}
+                />
+                <span>Search in description</span>
+            </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={this.state.searchInCategory}
+                    onChange={this.CheckBoxChanged('searchInCategory')}
+                />
+                <span>Search in category</span>
+            </label>
+        </div>
     </div>
     <table className="datagrid">
         <thead>
@@ -81,35 +153,17 @@ class Datagrid extends React.Component {
                 <th>#</th>
                 <th>
                     Title
-                    <span className="sort-icon">
-                        <FontAwesomeIcon icon="sort"/>
-                    </span>
                 </th>
-                <th onClick={() => {this.sort('category')}}>
+                <th>
                     Category
-                    <span className="sort-icon">
-                        {this.state.fields.category == null && <FontAwesomeIcon icon="sort"/>}               
-                        {this.state.fields.category == 'ASC' && <FontAwesomeIcon icon="sort-up"/>}            
-                        {this.state.fields.category == 'DESC' && <FontAwesomeIcon icon="sort-down"/>}               
-                    </span>
                 </th>
                 <th>
                     Quantity
-                    <span className="sort-icon">
-                        <FontAwesomeIcon icon="sort"/>
-                    </span>
                 </th>
-                <th
-                    >Description
-                    <span className="sort-icon">
-                        <FontAwesomeIcon icon="sort"/>
-                    </span>
+                <th>Description
                 </th>
                 <th>
                     Date Created
-                    <span className="sort-icon">
-                        <FontAwesomeIcon icon="sort"/>
-                    </span>
                 </th>
                 <th>
                     Action
@@ -117,7 +171,7 @@ class Datagrid extends React.Component {
             </tr>
         </thead>
         <tbody>
-            {this.state.products.map((product, index) => {
+            {this.state.filteredProducts.map((product, index) => {
                 return <tr key={product.Id}>
                 <td>{product.Id} </td>
                 <td>{product.title}</td>
@@ -126,7 +180,7 @@ class Datagrid extends React.Component {
                 <td>{product.description}</td>
                 <td>{product.creationDate}</td>
                 <td>
-                    <span className="btn-delete" onClick={() => this.handleOpenModal(index)}><FontAwesomeIcon icon="trash-alt"/></span>                    
+                    <span className="btn-delete" onClick={() => this.OpenModal(index)}><FontAwesomeIcon icon="trash-alt"/></span>                    
                 </td>
             </tr>
             })}            
@@ -145,8 +199,8 @@ class Datagrid extends React.Component {
         </div>
         <div className="modal-footer">
             <div className="float-right">                            
-                <button className="btn-submit danger" onClick={() => {this.deleteProduct()}}>Ok</button>
-                <button className="btn-cancel" onClick={this.handleCloseModal}>Cancel</button>
+                <button className="btn-submit danger" onClick={() => {this.DeleteProduct()}}>Ok</button>
+                <button className="btn-cancel" onClick={this.CloseModal}>Cancel</button>
             </div>
         </div>
     </Modal>
